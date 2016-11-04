f8c298a016ce1acc619ec3daeddc3f970f142cb4
Felix Loos Laptop <loos.felix93@gmail.com>
2016-11-04 22:29:16 +0100

    Added TestSuites

---
+++ src/test/java/org/wahlzeit/suites/AllTestsSuite.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        HandlersTestSuite.class,
+        ModelTestSuite.class,
+        ServicesTestSuite.class,
+        UtilsTestSuite.class
+})
+public class AllTestsSuite { }
---
+++ src/test/java/org/wahlzeit/suites/UtilsTestSuite.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.utils.StringUtilTest;
+import org.wahlzeit.utils.VersionTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        StringUtilTest.class,
+        VersionTest.class
+})
+public class UtilsTestSuite { }
