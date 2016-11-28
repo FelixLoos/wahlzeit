dfc91f949247dd0559a6cad8fb765cd0767213bd
Felix Loos Laptop <loos.felix93@gmail.com>
2016-11-28 13:07:19 +0100

    Added new tests for Spherical and Cartesian

--- src/test/java/org/wahlzeit/model/DifferentCoordinatesTest.java
+++ src/test/java/org/wahlzeit/model/DifferentCoordinatesTest.java
@@ -3,6 +3,7 @@ package org.wahlzeit.model;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test class for the interaction between the different Coordinate implementation classes.
@@ -21,5 +22,27 @@ public class DifferentCoordinatesTest {
         double result = cartesian1.getDistance(spheric1);
 
         assertEquals(expected, result, 0.01);
+    }
+
+    /**
+     * Test the equals method for two different Coordinates.
+     */
+    @Test
+    public void testEqualsWithCartesianAndSpherical() {
+        Coordinate cartesian = new CartesianCoordinate(1725.3483606172506, 956.3762131993382, 435.1667104522825);
+        Coordinate spherical = new SphericCoordinate(12.44, 29, 2020.112);
+
+        assertTrue(cartesian.equals(spherical));
+    }
+
+    /**
+     * Test the hashCode method for two different Coordinates.
+     */
+    @Test
+    public void testHashCodeWithCartesianAndSpherical() {
+        Coordinate cartesian = new CartesianCoordinate(1725.3483606172506, 956.3762131993382, 435.1667104522825);
+        Coordinate spherical = new SphericCoordinate(12.44, 29, 2020.112);
+
+        assertTrue(cartesian.hashCode() == spherical.hashCode());
     }
 }
